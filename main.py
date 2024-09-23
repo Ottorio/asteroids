dbf3e8bc0d3bb478ac53cc323b88985d341ceeb2
Ottorio <andrzej.gawron@interia.eu>
2024-09-23 22:18:02 +0200

    created player's ship

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame # type: ignore
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -12,7 +16,13 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
